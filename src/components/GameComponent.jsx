277f0952f579e45c26f3f7070f4b62bd20f59a25
Swathi Angadi <angadiswathi119@gmail.com>
2025-07-10 23:16:15 -0700

    Functionalities added

--- src/components/GameComponent.jsx
+++ src/components/GameComponent.jsx
@@ -3,31 +3,44 @@ import pastelButtonStyles from "./pastelButtonStyles";
 import clsx from "clsx";
 
 export default function GameComponent(){
-
+ 
     const colorChips = pastelButtonStyles;
-    const [currentWord, setCurrentWord] = useState("React".toUpperCase().split(""));
-    const alphabet= "abcdefghijklmnopqrstuvwxyz";
+    const [currentWord, setCurrentWord] = useState("react".split(""));
     const [guestArray, setGuestArray] = useState([]);
-    console.log(guestArray);
-
-    const dsiplayWord = currentWord.map((letter)=>
-        (<span key={letter} className="enteredLetter">{letter}</span>))
+  
+  
+    const alphabet= "abcdefghijklmnopqrstuvwxyz";
+    const wrongCount= guestArray.filter((item => !currentWord.includes(item))).length;
+   
+   // const isGameWon  = guestArray.filter((item)=>currentWord.includes(item)) && guestArray.length >= currentWord.length && wrongCount<8 ;
+   const isGameWon = currentWord.every((letter)=>guestArray.includes(letter)) 
+   console.log("iS game won", isGameWon);
+    const isGameLost = wrongCount >= pastelButtonStyles.length-1;
+    const isGameOver = isGameWon || isGameLost ;
+    const dsiplayWord = currentWord.map((letter)=>{
+        return (
+        <span key={letter} className="enteredLetter">
+            {guestArray.includes(letter) ? letter.toUpperCase(): ""}
+        </span>
+        )
+    });
 
     const display = colorChips.map((chip, index)=> {
+        let disappear =  wrongCount>index;
+      
         var Styles = {
             backgroundColor:chip.backgroundColor, 
             color:chip.textColor
         }
-       return <span className="colorChip"key={index} style={Styles}>{chip.language}</span>
-
+       return <span className={clsx("colorChip",{"lost":disappear})} key={index} id={index} style={Styles}>{chip.language}</span>
+       
     });
    
     const keyboard = alphabet.split("").map((key)=>{
        
          const isGuessed =  guestArray.includes(key);
-         const isCorrect = isGuessed && currentWord.includes(key.toUpperCase()) ;
-         const isWrong = isGuessed && !currentWord.includes(key.toUpperCase());
-         debugger;
+         const isCorrect = isGuessed && currentWord.includes(key) ;
+         const isWrong = isGuessed && !currentWord.includes(key);
         return (
             <button 
             key={key} 
@@ -39,8 +52,8 @@ export default function GameComponent(){
         )
     });
     
-    function handlekeyBoard(key){
-       
+    function handlekeyBoard(key){ 
+          
         setGuestArray((prev)=>{
          return   prev.includes(key)? prev :[...prev,key];
         //     const letrSet = new Set(prev)
@@ -48,15 +61,15 @@ export default function GameComponent(){
         //  return  Array.from(letrSet)
          
         })
-       
+      
     }
 
     return (
         <>
-        <span className="gameStatus">
-            You Win! <br/>
-            Well done 🎉
-        </span>
+        <div className="gameStatus">
+            <h4>You Win!</h4> 
+           <span> Well done 🎉</span>
+        </div>
         <section className="chips">
             {display}
         </section>
@@ -66,7 +79,7 @@ export default function GameComponent(){
         <section className="keySection">
             {keyboard}
         </section>
-        <button className="newGame"> New Game</button>
+        {isGameOver ? <button className="newGame"> New Game</button>: null}
         </>
     )
 }
