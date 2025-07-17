f5717f624da6c8ecaf872fda390fd63c10c44650
Swathi Angadi <angadiswathi119@gmail.com>
2025-07-16 18:40:51 -0700

    Changes reagrding hosting to github pages

--- src/components/pastelButtonStyles.js
+++ src/components/pastelButtonStyles.js
@@ -1,12 +1,12 @@
 const buttonStyles = [
-  { language: "JavaScript", textColor: "#000000", backgroundColor: "#f7df1e" },
-  { language: "React", textColor: "#ffffff", backgroundColor: "#b07219" },
-  { language: "NodeJS", textColor: "#ffffff", backgroundColor: "#178600" },  
-  { language: "Python", textColor: "#000000", backgroundColor: "#3572A5" },
+  { language: "HTML", textColor: "#000000", backgroundColor: "#f7df1e" },
+  { language: "CSS", textColor: "#ffffff", backgroundColor: "#b07219" },
+  { language: "JavaScript", textColor: "#ffffff", backgroundColor: "#178600" },  
+  { language: "React", textColor: "#000000", backgroundColor: "#3572A5" },
   { language: "Ruby", textColor: "#ffffff", backgroundColor: "#701516" },  
   { language: "TypeScript", textColor: "#ffffff", backgroundColor: "#3178C6" },
-  { language: "HTML", textColor: "#ffffff", backgroundColor: "#e34c26" },
-  { language: "CSS", textColor: "#ffffff", backgroundColor: "#563d7c" },
+  { language: "Python", textColor: "#ffffff", backgroundColor: "#e34c26" },
+  { language: "NodeJS", textColor: "#ffffff", backgroundColor: "#563d7c" },
   { language: "Assembly", textColor: "#000000", backgroundColor: "#8892BE" },
 ];
 export default buttonStyles;
